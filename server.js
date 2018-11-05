578d0f42b47d9a58bdf83ed2671d70facbd569ca
Minnacava <marinacamachovarea@outlook.com>
2018-11-05 17:02:00 +0100

    ultimo cambio server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const port = 8080;
+const port = process.env.PORT || 8080;
 // Serve only the static files form the dist directory
 
 app.use(express.static(path.join(__dirname, '/dist/testAppMarina/')));
