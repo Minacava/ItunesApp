ce7c0ad4b78c011db0b068788b5459b6092e4c89
Minnacava <marinacamachovarea@outlook.com>
2018-11-05 14:08:23 +0100

    updates to deply to heroku

---  server.js
+++  server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/<test-app-marina>'));
+app.use(express.static(__dirname + '/dist'));
 
-app.get('/*', function(req,res) {
+app.get('/*all', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/<test-app-marina>/index.html'));
+res.sendFile(path.join(__dirname+'/dis/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
